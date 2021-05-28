98235793a6ab4d5eb7a09376f2295ba1ce0020ee
s-deep3k <sankhadeep.genx@gmail.com>
2021-05-29 03:40:31 +0530

    Learning React- JSX and Components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>29th March 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">$297.67</div>
+    </div>
+  );
+}
+export default ExpenseItem;
