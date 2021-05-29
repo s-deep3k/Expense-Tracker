6c989163147b7b1f1684591c1fb34d6a224af553
s-deep3k <sankhadeep.genx@gmail.com>
2021-05-29 19:21:14 +0530

    Dynamic Outputting in JSX,CSS and Now to Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses =[
+    {
+      id:1,
+      title:'Car Insurance',
+      amount:239.46,
+      date: new Date(2021,6,4)
+
+    },
+    {
+      id:2,
+      title:'Toilet paper',
+      amount:4,
+      date: new Date(2021,3,1)
+
+    },
+    {
+      id:3,
+      title:'Book',
+      amount:33,
+      date: new Date(2021,6,4)
+
+    },
+    {
+      id:4,
+      title:'Iphone',
+      amount:999,
+      date: new Date(2021,9,11)
+
+    },
+    {
+      id:5,
+      title:'Loan',
+      amount:35661,
+      date: new Date(2021,0,1)
+
+    },
+  ]
   return (
     <div>
       <h2>Lets start React!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
