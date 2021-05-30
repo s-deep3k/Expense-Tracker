93538a7b0fa525164d8dd9d0e5520a124db59aa6
s-deep3k <sankhadeep.genx@gmail.com>
2021-05-30 22:47:06 +0530

    optimising expenses and props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const day = props.date.toLocaleString('en-US',{day:'numeric'})
-  const month = props.date.toLocaleString('en-US',{month:'long'})
-  const year = props.date.getFullYear()
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>  
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
